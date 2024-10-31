7f413a3417f47aaba0abe5b3f7384a3a25195dd0
Ashirli Gulustan <ashirliashirli1@gmail.com>
2024-10-31 22:55:25 +0400

    Task send to git

---
+++ noyabr/1/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Coding
+            /* Console.WriteLine("Salam");
+            Console.WriteLine("Adinizi daxil edin:");
+            string readLine = Console.ReadLine();
+            */
+            /* Console.Write("Salam, ");
+             Console.Write("Xos gelmissiniz ");
+             Console.ReadLine();
+             */
+            /* var red = "qirmizi";
+             var car = "masin";
+             var expensive = "bahali";
+             var phone = "telefon";
+             Console.WriteLine(red + " " + car);
+             Console.WriteLine(expensive + " " + phone);
+             Console.ReadLine();
+             */
+            // var read = Console.Read();
+            //var readKey = Console.ReadKey(); 
+            // var readLine = Console.ReadLine();
+            // Console.ReadLine();
+            /* var welcome = "Xos gorduk?";
+              var whatIsYourName = "Adiniz nedir?";
+             Console.WriteLine(welcome);
+             Console.WriteLine(whatIsYourName);
+             Console.ReadLine();
+            */
+            /* string person = "Gulustan Asirli";
+             Console.WriteLine(person);
+             string anotherPerson = "Ferid Memmedov";
+             Console.WriteLine(anotherPerson);
+             Console.ReadLine();
+             */
+            /*  string name = "Gulustan";
+              Console.WriteLine(name);
+              name = "Gulcin";
+              Console.WriteLine(name);
+              name = "Gunes";
+              Console.WriteLine(name);
+              Console.ReadLine();
+              */
+            #endregion
+
+            #region Task1
+            /* Console.WriteLine("Adiniz nedir?");
+             string nameOfUser = Console.ReadLine();
+
+             Console.WriteLine("Soyadiniz nedir?");
+             string surnameOfUser = Console.ReadLine();
+
+             Console.WriteLine("Hansi seherde yasyirsiniz?");
+             string cityOfUser = Console.ReadLine();
+
+             Console.ReadLine();
+
+             */
+            #endregion
+
+            #region Task1(vers2)
+
+            /* string name, surname, city;
+             Console.Write("Enter your name:");
+             name = Console.ReadLine();
+             Console.Write("Enter your surname:");
+             surname = Console.ReadLine();
+             Console.Write("Enter the city where you live:");
+             city = Console.ReadLine();
+             Console.WriteLine(name + " "+ surname + " "+ city);
+             Console.ReadLine();
+             */
+            #endregion
+
+            #region Task3
+            /*string city1, city2, city3, city4, city5, city6, city7, city8, city9, city10;
+            Console.WriteLine("Enter 10 city names:");
+            city1 = Console.ReadLine();
+            city2 = Console.ReadLine();
+            city3 = Console.ReadLine();
+            city4 = Console.ReadLine();
+            city5 = Console.ReadLine();
+            city6 = Console.ReadLine();
+            city7 = Console.ReadLine();
+            city8 = Console.ReadLine();
+            city9 = Console.ReadLine();
+            city10 = Console.ReadLine();
+
+            Console.WriteLine(city1 + " ," + city2 + " ," + city3 + "," + city4 + "," + city5 + "," + city6 + "," + city7 + "," + city8 + "," + city9 + "," + city10); 
+            
+            Console.ReadLine();
+            */
+
+            #endregion
+            #region Task2
+            /* string city1, city2, city3, city4, city5, city6, city7, city8, city9, city10;
+             Console.WriteLine("Enter 10 city names:");
+             city1 = Console.ReadLine();
+             city2 = Console.ReadLine();
+             city3 = Console.ReadLine();
+             city4 = Console.ReadLine();
+             city5 = Console.ReadLine();
+             city6 = Console.ReadLine();
+             city7 = Console.ReadLine();
+             city8 = Console.ReadLine();
+             city9 = Console.ReadLine();
+             city10 = Console.ReadLine();
+
+             Console.WriteLine(city1);
+             Console.WriteLine(city2);
+             Console.WriteLine(city3);
+             Console.WriteLine(city4);
+             Console.WriteLine(city5);
+             Console.WriteLine(city6);
+             Console.WriteLine(city7);
+             Console.WriteLine(city8);
+             Console.WriteLine(city9);
+             Console.WriteLine(city10);
+
+             Console.ReadLine();
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
